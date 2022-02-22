080579ed51bcfe9db2999da2eeae4bdd338777b1
Adalberto Bengaly <adalbertobengaly@gmail.com>
2022-02-22 19:54:05 -0300

    Introdução à testes com o Jest e Testing Library

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { TextInput } from '.'
+
+describe('<TextInput />', () => {
+    it('should have a value of searchValue', () => {
+        const fn = jest.fn()
+        render(<TextInput handleChange={fn} searchValue={'testando'} />)
+
+        const input = screen.getByPlaceholderText(/type your search/i)
+        expect(input).toBeInTheDocument()
+
+        expect(input.value).toBe('testando')
+    })
+
+    it('should call handleChange function on each key pressed', () => {
+        const fn = jest.fn()
+        render(<TextInput handleChange={fn} />)
+
+        const input = screen.getByPlaceholderText(/type your search/i)
+
+        const value = 'The Value is Here!'
+
+        userEvent.type(input, value)
+
+        expect(input.value).toBe(value)
+        expect(fn).toBeCalledTimes(value.length)
+    })
+
+    it('would match snapshot', () => {
+        const fn = jest.fn()
+        const { container } = render(<TextInput handleChange={fn} />)
+
+        expect(container).toMatchSnapshot()
+    }) 
+})
